8a8eab532c5999c20e88e65489de919fe35832f0
cabrera-evil <douglascabrera.dev@gmail.com>
2022-10-25 14:22:58 -0600

    Commit de prueba

---
+++ eje1.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+int main() {
+    int num, num1, num2, num3, num4, num5, num6, num7, num8, num9, num10;
+
+    printf("Diana Marisol Damas Coreas 00010021 24/10/2022");
+    scanf("Diana Marisol Damas Coreas 00010021 24/10/2022");
+
+    printf("Ingrese 10 numeros enteros?");
+
+    printf("Ingrese num1:");
+    scanf("%d", &num1);
+
+    printf("Ingrese num2:");
+    scanf("%d", &num2);
+
+    printf("Ingrese num3:");
+    scanf("%d", &num3);
+
+    printf("Ingrese num4:");
+    scanf("%d", &num4);
+
+    printf("Ingrese num5:");
+    scanf("%d", &num5);
+
+    printf("Ingrese num6:");
+    scanf("%d", &num6);
+
+    printf("Ingrese num7:");
+    scanf("%d", &num7);
+
+    printf("Ingrese num8:");
+    scanf("%d", &num8);
+
+    printf("Ingrese num9:");
+    scanf("%d", &num9);
+
+    printf("Ingrese num10:");
+    scanf("%d", &num10);
+
+
+    for ( i = 0; i < num; i++) {
+    printf("El numero es mayor");
+    }
+    
+    for ( i = 0; i < num; i--)
+    {
+    printf("El numero es menor");
+    }
+    
+
+
+    printf("Diana Marisol Damas Coreas 00010021 24/10/2022");
+return 0;
+
+
+}
---
+++ eje2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main (){;
+int año;
+
+printf("Diana Marisol Damas Coreas 00010021 24/10/2022");
+printf("Ingrese un año:");
+scanf("%d", &año)
+
+for ( i = 0; i < año; i++){
+    printf ("%d", i);
+}
+
+
+
+
+
+printf("Diana Marisol Damas Coreas 00010021 24/10/2022");
+
+}
